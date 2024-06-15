82a09b3aa97e2957f973391a9ac17be32bf83da4
Rajendra-Anargya <rajendra.anargya@gmail.com>
2024-06-15 15:26:15 +0700

    tes

--- resources/views/layouts/applandingpage.blade.php
+++ resources/views/layouts/applandingpage.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Keuangan Pribadi') }}</title>
 
     <!-- Favicons -->
     <link href="{{ asset('img/logo.png') }}" rel="icon">
