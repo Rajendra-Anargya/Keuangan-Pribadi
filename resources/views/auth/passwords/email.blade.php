28f9b10fffc307febbf9da09ada4032a45048c21
Rajendra-Anargya <rajendra.anargya@gmail.com>
2024-06-15 15:08:41 +0700

    edit

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -44,7 +44,7 @@
                     </div>
                 </div>
                 <div class="text-center mt-5 text-muted">
-                    Copyright &copy; {{ date('Y') }} &mdash; Kelompok 3 Gamelab
+                    Copyright &copy; {{ date('Y') }} &mdash;Rajendra Anargya
                 </div>
             </div>
         </div>
